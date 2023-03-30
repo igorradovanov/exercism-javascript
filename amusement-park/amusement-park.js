78144e7f60472f030ce44ec8a8cdedf0cf880d7f
Igor Radovanov <igor@radovanovigor.com>
2023-03-30 20:31:39 +0200

    Implement gtcVersion()

--- amusement-park/amusement-park.js
+++ amusement-park/amusement-park.js
@@ -69,5 +69,7 @@ export function simpleTicketStatus(tickets, ticketId) {
  * @returns {string | undefined} version
  */
 export function gtcVersion(visitor) {
-  throw new Error('Please implement the gtcVersion function.');
+  if(visitor.gtc?.version !== undefined){
+    return visitor.gtc.version;
+  }
 }

b50d6a49563531c583e156fab373b43e23440a6a
Igor Radovanov <igor@radovanovigor.com>
2023-03-31 20:12:44 +0200

    Implement quantities()

--- lasagna-master/lasagna-master.js
+++ lasagna-master/lasagna-master.js
@@ -11,6 +11,12 @@
  * }
  */
 
+/**
+ * Returns cooking status based on time
+ * 
+ * @param {number} time 
+ * @returns cooking status
+ */
 
 export function cookingStatus(time) {
     if (time == 0) {
@@ -22,6 +28,37 @@ export function cookingStatus(time) {
     }
 }
 
-export function preparationTime(layers, time=2){
+/**
+ * Returns preparation time in minutes
+ * 
+ * @param {array} layers 
+ * @param {number} time 
+ * @returns {number} preparation time
+ */
+export function preparationTime(layers, time = 2) {
     return layers.length * time;
+}
+
+/**
+ * Calculates the quantities or layers
+ * 
+ * @param {array} layers 
+ * @returns {object} quantities of noodles and sauces
+ */
+export function quantities(layers) {
+    const NOODLE_LAYER_GRAMS = 50;
+    const SAUCE_LAYER_LITERS = 0.2;
+
+    let noodles_total = 0;
+    let sauce_total = 0;
+
+    for (let layer of layers) {
+        if (layer == 'noodles') {
+            noodles_total += NOODLE_LAYER_GRAMS;
+        } else if (layer == 'sauce') {
+            sauce_total += SAUCE_LAYER_LITERS;
+        }
+    }
+
+    return { noodles: noodles_total, sauce: sauce_total };
 }

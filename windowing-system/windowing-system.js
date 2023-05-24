99c98989bdc0e9b2891d24d54dfe171b4132e29a
Igor Radovanov <igor@radovanovigor.com>
2023-05-24 21:02:56 +0200

    Implement Position Class

--- windowing-system/windowing-system.js
+++ windowing-system/windowing-system.js
@@ -18,7 +18,17 @@ export function Size(width = 80, height = 60) {
     this.height = height;
 }
 
-Size.prototype.resize = function (width, height) {
-    this.width = width;
-    this.height = height;
+Size.prototype.resize = function (newWidth, newHeight) {
+    this.width = newWidth;
+    this.height = newHeight;
+}
+
+export function Position(x = 0, y = 0){
+    this.x = x;
+    this.y = y;
+}
+
+Position.prototype.move= function(newX, newY){
+    this.x = newX;
+    this.y = newY;
 }

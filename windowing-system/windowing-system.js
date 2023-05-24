8c0722b70a6ec3ec44b5a22ba82e76d0e78fd491
Igor Radovanov <igor@radovanovigor.com>
2023-05-24 21:16:34 +0200

    Implement ProgramWindow class

--- windowing-system/windowing-system.js
+++ windowing-system/windowing-system.js
@@ -23,12 +23,32 @@ Size.prototype.resize = function (newWidth, newHeight) {
     this.height = newHeight;
 }
 
-export function Position(x = 0, y = 0){
+/**
+ * Defines the Position class using the function prototype
+ * 
+ * @param {number} x 
+ * @param {number} y 
+ */
+
+export function Position(x = 0, y = 0) {
     this.x = x;
     this.y = y;
 }
 
-Position.prototype.move= function(newX, newY){
+Position.prototype.move = function (newX, newY) {
     this.x = newX;
     this.y = newY;
+}
+
+/**
+ * Defines the ProgramWind class with the default constructor
+ * 
+ */
+
+export class ProgramWindow {
+    constructor() {
+        this.screenSize = new Size(800, 600);
+        this.size = new Size();
+        this.position = new Position();
+    }
 }
